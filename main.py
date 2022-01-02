5670344bc1fbbfa59253f3e80ccfe10362645f07
Labitoku <louis.euvrardarnaud@gmail.com>
2022-01-02 14:42:53 +0100

    Définition de premières fonctions pour se souvenir de Pillow

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-import PIL
+from PIL import Image
 import sys
+import os
+
+
+def importImage(path):
+    image = Image.open(path)
+    return image
+
+
+def showImage(im):
+    im.show()
+
+
+def saveImage(im, path):
+    im.save(path)
+
+
+
 
 
 def main():
     print("Argv length : " + str(len(sys.argv)))
+
+
 
 if __name__ == '__main__':
     main()
